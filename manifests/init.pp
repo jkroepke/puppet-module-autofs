5fe82734717006644835a41094d60873052ca7d9
Rehan Mahmood <rehanone@gmail.com>
2018-05-05 09:57:30 -0400

    - Converted `params.pp` to hiera5 modle data pattern.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,25 +1,26 @@
 # Class: autofs
 
 class autofs (
-  String $config_file_owner            = $autofs::params::config_file_owner,
-  String $config_file_group            = $autofs::params::config_file_group,
-  String $master_config                = $autofs::params::master_config,
-  Stdlib::Absolutepath $map_config_dir = $autofs::params::map_config_dir,
-  Boolean $package_manage              = $autofs::params::package_manage,
-  Array[String] $package_name          = $autofs::params::package_name,
-  String $package_ensure               = $autofs::params::package_ensure,
-  String $service_name                 = $autofs::params::service_name,
-  Boolean $service_manage              = $autofs::params::service_manage,
-  Boolean $service_hasrestart          = $autofs::params::service_hasrestart,
-  Boolean $service_hasstatus           = $autofs::params::service_hasstatus,
-  Variant[Enum[running, stopped], Boolean]
-  $service_ensure                      = $autofs::params::service_ensure,
-  Boolean $service_enable              = $autofs::params::service_enable,
-  Optional[String] $service_restart    = $autofs::params::service_restart,
-  Hash $mapfiles                       = {},
-  Hash $mounts                         = {},
-  Hash $includes                       = {},
-) inherits autofs::params {
+  String $config_file_owner,
+  String $config_file_group,
+  String $master_config,
+  Stdlib::Absolutepath $map_config_dir,
+  Array[String] $supported_map_types,
+  Boolean $use_map_prefix,
+  Boolean $package_manage,
+  Array[String] $package_name,
+  String $package_ensure,
+  String $service_name,
+  Boolean $service_manage,
+  Variant[Enum[running, stopped], Boolean] $service_ensure,
+  Boolean $service_enable,
+  Boolean $service_hasrestart,
+  Boolean $service_hasstatus,
+  Optional[String] $service_restart,
+  Hash $mapfiles = {},
+  Hash $mounts   = {},
+  Hash $includes = {},
+) {
 
   # Anchor this as per #8040 - this ensures that classes won't float off and
   # mess everything up.  You can read about this at:
